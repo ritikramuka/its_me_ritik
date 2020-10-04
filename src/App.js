4545eb55f4c4ab21e98d9e3d25e58e1e5114344b
ritikramuka <ritik.ramuka@gmail.com>
2020-10-04 13:00:50 +0530

    navbar layout made && some router things

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="app">
-      <div className="container app__container">
-        <div className="row app__row">
-          <div className="col-lg-3 app__sidebar">
-            <Sidebar />
+    <Router>
+      <div className="app">
+        <div className="container app__container">
+          <div className="row app__row">
+            <div className="col-lg-3 app__sidebar">
+              <Sidebar />
+            </div>
+            <div className="col-lg-9 app__content">
+              <Navbar></Navbar>
+              <Switch>
+                <Route exact path="/">hwloo</Route>
+                <Route exact path="/resume">heya</Route>
+                <Route exact path="/projects">hanji</Route>
+              </Switch>
+            </div>
           </div>
-          <div className="col-lg-9 app__content">Hello xD</div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [nav, setNav] = useState("About");
+  useEffect(() => {
+    const url = window.location.href;
+  }, [nav]);
+
+  return (
+    <div className="navbar">
+      <div className="navbar__active">{nav}</div>
+      <div className="navbar__items">
+        {nav !== "About" ? (
+          <Link to="./">
+            <div className="navbar__item" onClick={() => setNav("About")}>
+              About
+            </div>
+          </Link>
+        ) : null}
+        {nav !== "Resume" ? (
+          <Link to="./resume">
+            <div className="navbar__item" onClick={() => setNav("Resume")}>
+              Resume
+            </div>
+          </Link>
+        ) : null}
+        {nav !== "Projects" ? (
+          <Link to="./projects">
+            <div className="navbar__item" onClick={() => setNav("Projects")}>
+              Projects
+            </div>
+          </Link>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
